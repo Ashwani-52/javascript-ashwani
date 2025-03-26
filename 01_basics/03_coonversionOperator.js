d60ea5c1e98cb997a739bd9d401b16c918971f35
Ashwani Kumar <164582007+Ashwani-52@users.noreply.github.com>
2025-03-26 17:50:16 +0000

    day 3

---
+++ 01_basics/03_coonversionOperator.js
@@ -0,0 +1,26 @@
+//let score = "33"
+//console.log(typeof score)
+
+//let valueInNumber = Number(score) //use to convert string to number
+//console.log(typeof valueInNumber)
+
+/*
+"33" => 33
+"33abc" => NaN (not a number)
+true => 1
+false => 0
+*/
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+/*
+1 => true; 0 => false
+" " => false; "rishab" => true
+*/
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
