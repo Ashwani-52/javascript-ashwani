91f5c4630c6121749597446f5c315e828139ec77
Ashwani Kumar <164582007+Ashwani-52@users.noreply.github.com>
2025-03-27 18:48:35 +0000

    day 4

---
+++ 01_basics/strings.js
@@ -0,0 +1,14 @@
+const name = "rishav"
+const repoCount = 50
+// console.log(name + repoCount + " Value");
+
+//modern way 
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`)
+
+// new way to declare string
+const gameName = new String('rishab')
+console.log(gameName[0])
+console.log(gameName.__proto__);
+
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
