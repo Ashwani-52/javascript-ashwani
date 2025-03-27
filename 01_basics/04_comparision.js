bc90e059673e1300af72081b095633dcad378fbb
Ashwani Kumar <164582007+Ashwani-52@users.noreply.github.com>
2025-03-27 17:53:46 +0000

    day 4

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,18 @@
+// console.log(2>1)
+// console.log(2==1)
+// console.log(2!=1)
+// console.log(2<1)
+// console.log(2>=1)
+
+// console.log("2">1)
+// console.log("02">1)
+
+//alwayss avoid, clean code ko priority do
+// console.log(null > 0) // null = 0
+// console.log(null == 0) // null = 0
+// console.log(null >= 0) // treating as number null >= 0
+
+// *********************
+console.log("2" === 2) //=== strictly check, its datatype also
+
+
